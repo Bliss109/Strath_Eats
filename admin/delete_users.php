2599dd1a9ff148eb77a3ac3ed52093a819588b5b
ndiranguph <166705736+ndiranguph@users.noreply.github.com>
2024-11-17 15:57:50 +0300

    Fixed sql inconsistencies

--- admin/delete_users.php
+++ admin/delete_users.php
@@ -15,22 +15,13 @@ if ($conn->connect_error) {
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
-    $delete_recipes_query = "DELETE FROM users WHERE user_id = $id";
-    if ($conn->query($delete_recipes_query) === TRUE) {
-        $delete_type_query = "DELETE FROM user_types WHERE user_id = $id";
-        if ($conn->query($delete_type_query) === TRUE) {
-            $delete_user_query = "DELETE FROM users WHERE id = $id";
-            if ($conn->query($delete_user_query) === TRUE) {
-                header("Location: display_users.php");
-                exit();
-            } else {
-                echo "Error deleting user: " . $conn->error;
-            }
-        } else {
-            echo "Error deleting user type: " . $conn->error;
-        }
+    $delete_user_query = "DELETE FROM users WHERE user_id = $id";
+
+    if ($conn->query($delete_user_query) === TRUE) {
+        header("Location: display_users.php");
+        exit();
     } else {
-        echo "Error deleting recipes: " . $conn->error;
+        echo "Error deleting user: " . $conn->error;
     }
 } else {
     echo "No user ID provided.";
@@ -39,3 +30,4 @@ if (isset($_GET['id'])) {
 
 $conn->close();
 ?>
+
